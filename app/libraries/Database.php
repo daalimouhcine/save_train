f7eb614883040a3202b91c9ea43063ecd9984f37
Mouhcine Daali <daalim277@gmail.com>
2022-02-17 23:49:45 +0100

    set up the database class

---
+++ app/libraries/Database.php
@@ -0,0 +1,11 @@
+<?php
+    /* 
+     * PDO Database Class
+     * Connect to database
+     * Create prepared statements
+     * Bind values
+     * Return rows and results
+    */
+    class Database {
+        
+    }
