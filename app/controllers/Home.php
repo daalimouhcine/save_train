402ace99dcb4aff3ea601c8811635060e40c7e91
Mouhcine Daali <daalim277@gmail.com>
2022-02-18 23:03:39 +0100

    end the set up of the mvc

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,26 +1,23 @@
 <?php
-    class Pages extends Controller{
+    class Home extends Controller{
         
         public function __construct() {
-            $this->postModel = $this->model('Post');
+           
         }
         
         public function index() {
-            $posts = $this->postModel->getPosts();
-
             $data = [
                 'title' => 'WELCOME',
-                'posts' => $posts
             ];
 
-            $this->view('pages/index', $data);
+            $this->view('home/index', $data);
         }
 
         public function about(){
             $data = [
                 'content' => 'FIRST ABOUT IN THE WOOORLD'
             ];
-            $this->view('pages/about', $data);
+            $this->view('home/about', $data);
 
         }
 
