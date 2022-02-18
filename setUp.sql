a0ece60a247698ef7fb9815251fb50fcf50fa98a
Mouhcine Daali <daalim277@gmail.com>
2022-02-18 10:34:41 +0100

    modify and add query to insert some data in the tables

--- setUp.sql
+++ setUp.sql
@@ -55,3 +55,22 @@ CREATE TABLE IF NOT EXISTS reservation_tickets(
     FOREIGN KEY (id_guest) REFERENCES guests(id)
 );
 
+
+INSERT INTO `admins`(`fullName`, `email`, `password`) 
+        VALUES ('mouhcine daali','admine@savetrain.com','admine');
+
+INSERT INTO `clients`(`fullName`, `email`, `password`, `phoneNumber`, `birthDay`) 
+        VALUES ('abdsalam staili','abdsalam@gmail.com','abdsalamstaili','0634175255','2003/02/15');
+
+INSERT INTO `guests`(`fullName`, `email`, `phoneNumber`, `birthDay`) 
+        VALUES ('amine mesbahi','amine@gmail.com','0660536586','2002/08/15');
+
+INSERT INTO `reservation_tickets`(`id_trip`, `id_client`, `id_guest`) 
+        VALUES ('[value-1]','[value-2]','[value-3]');
+
+INSERT INTO `trains`(`train_type`, `seat_number`) 
+        VALUES ('train_type','50');
+
+INSERT INTO `train_trips`(`train_id`, `start_from`, `end_in`, `distance`, `trip_date`, `trip_time`, `depart_hour`, `end_hour`, `class`, `price`) 
+        VALUES ('train_id','start_from','end_in','distance','trip_date','trip_time','depart_hour','end_hour','class','price');
+
