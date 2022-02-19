627dd4090646be71ebdae3e11a8d23c2e3a76f30
Mouhcine Daali <daalim277@gmail.com>
2022-02-20 00:08:43 +0100

    start the database logic of the users controller

--- app/views/home/about.php
+++ app/views/home/about.php
@@ -1,3 +1,9 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
-    <h1><?= $data['content']; ?></h1>
+
+    <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
+
+    <div class="container">
+        <h1><?= $data['content']; ?></h1>
+    </div>
+
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
--- app/views/user/Register.php
+++ app/views/user/Register.php
@@ -1,7 +1,10 @@
-<?php require_once APPROOT . '/views/inc/header.php'; 
-        require_once APPROOT . '/views/inc/navbar.php';
-?>
+<?php require_once APPROOT . '/views/inc/header.php'; ?>
 
-    <h1><?=$data['title'];?></h1>
+    <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
+
+    <div class="container">
+        <h1><?=$data['title'];?></h1>
+    </div>
+
 
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
