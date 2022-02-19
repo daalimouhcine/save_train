4b70f1cb99c8ee693b3ba39e7c58a0c48d2e1bf6
Mouhcine Daali <daalim277@gmail.com>
2022-02-19 13:45:20 +0100

    set up the home page

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= SITENAME;?></title>
-    <link rel="stylesheet" href="<?= URLROOT; ?>/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">    <link rel="stylesheet" href="<?= URLROOT; ?>/style/style.css">
 </head>
 <body>
+    <div class="container">
      
+    
