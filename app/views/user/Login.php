27e5b915431d91f91e2c078ce31ef95216bcd1f0
Mouhcine Daali <daalim277@gmail.com>
2022-02-20 23:51:10 +0100

    modify

--- app/views/user/Login.php
+++
@@ -1,9 +0,0 @@
-<?php require_once APPROOT . '/views/inc/header.php'; ?>
-
-    <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
-
-    <div class="container">
-        <h1><?=$data['title'];?></h1>
-    </div>
-
-<?php require_once APPROOT . '/views/inc/footer.php'; ?>
