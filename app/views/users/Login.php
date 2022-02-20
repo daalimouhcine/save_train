a641e523ab1cb3bfa1e653ffd61688117c05672b
Mouhcine Daali <daalim277@gmail.com>
2022-02-20 23:53:37 +0100

    delete autocomplete attribut

--- app/views/users/Login.php
+++ app/views/users/Login.php
@@ -7,12 +7,12 @@
                 <form action="<?= URLROOT; ?>/users/login" method="POST">
                     <div class="form-group">
                         <label for="email">Email: <sup>*</sup></label>
-                        <input type="email" name="email" class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" autocomplete="off" value="<?= $data['email']; ?>" >
+                        <input type="email" name="email" class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['email']; ?>" >
                         <span class="invalid-feedback"><?= $data['email_err']; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="password">Password: <sup>*</sup></label>
-                        <input type="password" name="password" class="form-control form-control-lg <?= (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" autocomplete="off" value="<?= $data['password']; ?>" >
+                        <input type="password" name="password" class="form-control form-control-lg <?= (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['password']; ?>" >
                         <span class="invalid-feedback"><?= $data['password_err']; ?></span>
                     </div>
 
