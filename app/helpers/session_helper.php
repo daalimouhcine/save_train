64e95061326b64b21a2d88aefdeafa559f11121e
Mouhcine Daali <daalim277@gmail.com>
2022-02-22 00:16:47 +0100

    end register success session script

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -3,6 +3,25 @@
 
     // Flash message helper
     // Display in view
-    function flash($name = '') {
+    function flash($name = '', $message = '', $class = 'alert alert-success') {
+        if(!empty($name)) {
+            if(!empty($message) && empty($_SESSION[$name])) {
+                if(!empty($_SESSION[$name])) {
+                    unset($_SESSION[$name]);
+                }
 
+                if(!empty($_SESSION[$name . '_class'])) {
+                    unset($_SESSION[$name . '_class']);
+                }
+
+                $_SESSION[$name] = $message;
+                $_SESSION[$name . '_class'] = $class;
+
+            } elseif(empty($message) && !empty($_SESSION[$name])) {
+                $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+                echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
+                unset($_SESSION[$name]);
+                unset($_SESSION[$name . '_class']);
+            }
+        }
     }
