2cb9f9b7863c6147993220a4dfa7be8b9d63bd13
Mouhcine Daali <daalim277@gmail.com>
2022-02-22 00:17:19 +0100

    modify login page to display alert

--- app/views/users/Login.php
+++ app/views/users/Login.php
@@ -3,6 +3,7 @@
     <div class="row">
         <div class="col-md-6 mx-auto">
             <div class="card card-body bg-light mt-5">
+                <?php flash('register_success'); ?>
                 <h2>Login</h2>
                 <p>Please fill in your credentials to log in</p>
                 <form action="<?= URLROOT; ?>/users/login" method="POST">
