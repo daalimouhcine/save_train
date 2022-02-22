c5b863b9255c5a8f56ff2a773f72e9964b5c5dd3
Mouhcine Daali <daalim277@gmail.com>
2022-02-23 00:00:48 +0100

    modify navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,50 +1,121 @@
 
 <?php if(isset($_SESSION['admin_id'])) : ?>
-  
-<?php else : ?>
+  <div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="sidebar-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Orders
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Products
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="bar-chart-2"></span>
+              Reports
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Integrations
+            </a>
+          </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Saved reports</span>
+          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Current month
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Last quarter
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Social engagement
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Year-end sale
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+  <?php else : ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+      <div class="container">
+            <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+      
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
+                </li>
+              </ul>
+              
+              <ul class="navbar-nav ml-auto">
+                <?php if(isset($_SESSION['user_id'])) : ?>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+                  </li>         
+    
+                <?php else : ?>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+                  </li>
+                  
+                <?php endif; ?>
+              </ul>
+            </div>
+          </div>
+      </nav>
 
 <?php endif; ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-  <div class="container">
-        <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-  
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
-            </li>
-          </ul>
-          
-          <ul class="navbar-nav ml-auto">
-            <?php if(isset($_SESSION['user_id'])) : ?>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
-              </li>
-
-            <?php elseif (isset($_SESSION['admin_id'])) : ?>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href=""><strong>Admine</strong></a>
-              </li>           
-
-            <?php else : ?>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
-              </li>
-            <?php endif; ?>
-          </ul>
-        </div>
-      </div>
-  </nav>
   
