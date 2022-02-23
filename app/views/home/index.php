dbe19b995e16f86c564e40a4ba062580328a444d
Mouhcine Daali <daalim277@gmail.com>
2022-02-23 23:53:37 +0100

    add links on the a tage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -12,7 +12,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="<?= URLROOT; ?>">
                   <span data-feather="file"></span>
                   <i class="fas fa-subway"></i>
                   Trains
