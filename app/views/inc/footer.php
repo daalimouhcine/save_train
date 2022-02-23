5196bd8816bcad35059c9a4d0875e8e9174669ae
Mouhcine Daali <daalim277@gmail.com>
2022-02-23 23:56:40 +0100

    delete the close tage for main

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,4 @@
 
-        </main> 
         <?php if(isset($_SESSION['client_id'])) : ?>
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <p class="col-md-4 mb-0 text-muted">&copy; 2022 Savetrain, Inc</p>
