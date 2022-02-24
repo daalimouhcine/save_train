079a36c013784b997aa55ef4ba55f5d8e62d9727
Mouhcine Daali <daalim277@gmail.com>
2022-02-24 11:59:35 +0100

    modify the index of the trains

--- app/views/trains/index.php
+++ app/views/trains/index.php
@@ -50,8 +50,37 @@
           </div>
         </nav>
 
+
+
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-          
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 class="h2">Trains</h1>
+          <div class="btn-toolbar mb-2 mb-md-0">
+            <a href="<?= URLROOT; ?>/trains/add" type="button" class="btn  btn-success">Add Train</a>
+        </div>
+      </div>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                  <td>text</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </main>
       </div>
     </div>
