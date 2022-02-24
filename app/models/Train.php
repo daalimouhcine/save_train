eb21af70d6a501412cef62448459bfd47c628c9a
Mouhcine Daali <daalim277@gmail.com>
2022-02-24 11:58:29 +0100

    add method to insert new trains to the database

--- app/models/Train.php
+++ app/models/Train.php
@@ -7,7 +7,28 @@
 
         }
 
-        public function readTrains() {
+        // public function readTrains() {
+        //     $this->db->trainMethod->query('SELECT * FROM trains');
+        //     $trains = $this->db->trainMethod->execute();
+        //     $row = $this->db->rowCount();
+
+        //     if($row) {
+        //         return $trains;
+        //     } else {
+        //         return false;
+        //     }
+        // }
+
+
+        public function addTrain($data) {
+            $this->db->query("INSERT INTO trains(name, seat_number) VALUES(:name, :seat_number)");
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':seat_number', $data['seat_number']);
             
+            if($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
