5d111d69c91f852b10ea2212d5b7c0ad51004504
Mouhcine Daali <daalim277@gmail.com>
2022-02-25 15:14:40 +0100

    display the flash message of the delete train functionality

--- app/views/trains/index.php
+++ app/views/trains/index.php
@@ -55,9 +55,10 @@
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Trains</h1>
-          
           <?php flash('train_add_success'); ?>
           <?php flash('no_trains'); ?>
+          <?php flash('train_delete_success'); ?>
+          <?php flash('delete_prob'); ?>
           <div class="btn-toolbar mb-2 mb-md-0">
             <a href="<?= URLROOT; ?>/trains/add" type="button" class="btn  btn-success">Add Train</a>
         </div>
@@ -66,7 +67,7 @@
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
-                  <th>Name</th>
+                  <th class="px-5">Name</th>
                   <th>Seat Number</th>
                   <th>Params</th>
                 </tr>
@@ -74,9 +75,12 @@
               <tbody>
                 <?php foreach($data as $train) : ?>
                   <tr>
-                    <td><?= $train['name']; ?></td>
-                    <td><?= $train['seat_number']; ?></td>
-                    <td>text</td>
+                    <td class="w-25 px-5"><?= $train['name']; ?></td>
+                    <td class="w-25"><?= $train['seat_number']; ?></td>
+                    <td class="w-25">
+                      <a href="<?= URLROOT; ?>/trains/edit/<?= $train['id']; ?>" class="btn btn-outline-success">Edit</a>
+                      <a href="<?= URLROOT; ?>/trains/delete/<?= $train['id']; ?>" class="btn btn-outline-danger">Delete</a>
+                    </td>
                   </tr>
                 <?php endforeach; ?>
               </tbody>
