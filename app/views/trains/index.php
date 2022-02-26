a4772abd78557376eacacb2c056875180f8480e3
Mouhcine Daali <daalim277@gmail.com>
2022-02-27 00:52:30 +0100

    modify the links to train page

--- app/views/trains/index.php
+++ app/views/trains/index.php
@@ -6,13 +6,13 @@
           <div class="sidebar-sticky pt-3">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link" href="<?= URLROOT; ?>">
                   <span data-feather="home"></span>
                   Dashboard <span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="<?= URLROOT; ?>">
+                <a class="nav-link active" href="<?= URLROOT; ?>/trains/">
                   <span data-feather="file"></span>
                   <i class="fas fa-subway"></i>
                   Trains
