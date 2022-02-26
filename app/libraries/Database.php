fce5570196312146531e197273138c4e9e40c456
Mouhcine Daali <daalim277@gmail.com>
2022-02-26 22:52:13 +0100

    fetch assoc to obj

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -68,7 +68,7 @@
         // Get result set as array of objects
         public function resultSet() {
             $this->execute();
-            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
         }
 
         // Get single record as object
