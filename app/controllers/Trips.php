42bf45b0990ce07b93e7baa7e007be4f62fce6b9
Mouhcine Daali <daalim277@gmail.com>
2022-02-28 00:36:46 +0100

    start working on the add method

--- app/controllers/Trips.php
+++ app/controllers/Trips.php
@@ -1,8 +1,8 @@
 <?php
-    class Trips extends Controller{
+    class Trips extends Controller {
 
         public function __construct() {
-            $this->tripModel = $this->model('Trip');
+            // $this->tripModel = $this->model('Trip');
         }
 
 
@@ -13,10 +13,12 @@
 
 
         public function add() {
+            // Check for POST
             if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
             } else {
+                // Init data
                 $data = [
                     'train' => '',
                     'start_from' => '',
@@ -28,7 +30,19 @@
                     'end_hour' => '',
                     'class' => '',
                     'price' => '',
-                ]
+                    'train_err' => '',
+                    'start_from_err' => '',
+                    'end_in_err' => '',
+                    'distance_err' => '',
+                    'trip_date_err' => '',
+                    'trip_time_err' => '',
+                    'depart_hour_err' => '',
+                    'end_hour_err' => '',
+                    'class_err' => '',
+                    'price_err' => ''
+                ];
+                // Load view
+                $this->view('trips/add', $data);
             }
 
         }
