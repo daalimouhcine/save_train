1dc1b4a2b5b2f7faff3e25d8002bcc9ff8f83922
Mouhcine Daali <daalim277@gmail.com>
2022-02-28 21:22:57 +0100

    change some column from int to varchar and delete som inserts

--- setUp.sql
+++ setUp.sql
@@ -33,10 +33,10 @@ CREATE TABLE IF NOT EXISTS train_trips(
     train_id int,
     start_from varchar(255) NOT NULL,
     end_in varchar(255) NOT NULL,
-    distance int,
-    trip_date int NOT NULL,
-    depart_hour int NOT NULL,
-    end_hour int NOT NULL,
+    distance varchar(255),
+    trip_date varchar(255) NOT NULL,
+    depart_hour varchar(255) NOT NULL,
+    end_hour varchar(255) NOT NULL,
     price double NOT NULL,
     FOREIGN KEY (train_id) REFERENCES trains(id)
 );
@@ -54,19 +54,4 @@ CREATE TABLE IF NOT EXISTS reservation_tickets(
 
 INSERT INTO `admins`(`fullName`, `email`, `password`) 
         VALUES ('mouhcine daali','admin@savetrain.com','$2y$10$Y2LUd3kM4iqsOBwLA5U23OKU5kbROTq5CVmypCAg9.sdd.ho7gslW');
-
-INSERT INTO `clients`(`fullName`, `email`, `password`, `phoneNumber`, `birthDay`) 
-        VALUES ('abdsalam staili','abdsalam@gmail.com','abdsalamstaili','0634175255','2003/02/15');
-
-INSERT INTO `guests`(`fullName`, `email`, `phoneNumber`, `birthDay`) 
-        VALUES ('amine mesbahi','amine@gmail.com','0660536586','2002/08/15');
-
-INSERT INTO `reservation_tickets`(`id_trip`, `id_client`, `id_guest`) 
-        VALUES ('[value-1]','[value-2]','[value-3]');
-
-INSERT INTO `trains`(`train_type`, `seat_number`) 
-        VALUES ('train_type','50');
-
-INSERT INTO `train_trips`(`train_id`, `start_from`, `end_in`, `distance`, `trip_date`, `depart_hour`, `end_hour`, `price`) 
-        VALUES ('train_id','start_from','end_in','distance','trip_date','depart_hour','end_hour','price');
 
