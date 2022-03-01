05a4658b2981b30d788ebd92a950440e05e8ce5e
Mouhcine Daali <daalim277@gmail.com>
2022-03-01 15:16:04 +0100

    modify the data variable

--- app/controllers/Trips.php
+++ app/controllers/Trips.php
@@ -114,7 +114,8 @@
                 // Init data
                 $data = [
                     'trains_available' => $trains,
-                    'train' => '',
+                    'train_name' => '',
+                    'train_id' => '',
                     'start_from' => '',
                     'end_in' => '',
                     'distance' => '',
@@ -139,11 +140,119 @@
 
 
         public function edit($trip_id) {
+            // Get the trains
+            $trains = $this->trainModel->readTrains();
+            // Check for POST request
             if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $row = $this->trainModel->getOneTrain($_POST['train']);
+                // Init data
+                $data = [
+                    'trains_available' => $trains,
+                    'train_name' => $row->name,
+                    'train_id' => $_POST['train'],
+                    'start_from' => $_POST['start_from'],
+                    'end_in' => $_POST['end_in'],
+                    'distance' => $_POST['distance'],
+                    'trip_date' => $_POST['trip_date'],
+                    'depart_hour' => $_POST['depart_hour'],
+                    'end_hour' => $_POST['end_hour'],
+                    'price' => $_POST['price'],
+                    'train_err' => '',
+                    'start_from_err' => '',
+                    'end_in_err' => '',
+                    'distance_err' => '',
+                    'trip_date_err' => '',
+                    'depart_hour_err' => '',
+                    'end_hour_err' => '',
+                    'price_err' => ''
+                ];
+                
+                
+                // Validate train
+                if(empty($data['train_name']) || empty($data['train_id'])) {
+                    $data['train_err'] = 'Pleas select a train';
+                }
+
+                // Validate start_from
+                if(empty($data['start_from'])) {
+                    $data['start_from_err'] = 'Pleas enter where the start is gonna be';
+                }
+
+                // Validate end_in
+                if(empty($data['end_in'])) {
+                    $data['end_in_err'] = 'Pleas enter where the end is gonna be';
+                }
+
+                // Validate distance
+                if(empty($data['distance'])) {
+                    $data['distance_err'] = 'Pleas enter the distance';
+
+                } elseif($data['distance'] <= 0) {
+                    $data['distance_err'] = "( $data[distance] ) is not a valid distance";
+                }
+
+                // Validate trip_date
+                if(empty($data['trip_date'])) {
+                    $data['trip_date_err'] = 'Pleas enter the Date of the trip';
+                }
+
+                // Validate depart_hour
+                if(empty($data['depart_hour'])) {
+                    $data['depart_hour_err'] = 'Pleas enter the Hour of the depart';
+                }
+
+                // Validate end_hour
+                if(empty($data['end_hour'])) {
+                    $data['end_hour_err'] = 'Pleas enter the Hour of the end';
+                }
+                                
+                // Validate end_hour
+                if(empty($data['price'])) {
+                    $data['price_err'] = 'Pleas enter the price';
+
+                } elseif($data['price'] <= 0) {
+                    $data['price_err'] = "( $data[price] ) is not a valid price";
+                }
+
+                
+                // Mack sure all errors arr empty
+                if(empty($data['train_err']) && empty($data['start_from_err']) && empty($data['end_in_err']) && empty($data['distance_err']) && empty($data['trip_date_err']) && empty($data['depart_hour_err']) && empty($data['end_hour_err']) && empty($data['price_err'])) {
+                    if($this->tripModel->edit($data, $trip_id)) {
+                        flash('modify_trip', 'Trip modified successfully');
+                        redirect('trips/');
+                    }
+                    
+                } else {
+                    $this->view('trips/modify', $data);
+                }
 
 
             } else {
-                $this->tripMethod->readTrips();
+                $trip = $this->tripModel->getOnTrip($trip_id);
+                $data = [
+                    'trains_available' => $trains,
+                    'train_name' => '',
+                    'train_id' => $trip->train_id,
+                    'start_from' => $trip->start_from,
+                    'end_in' => $trip->end_in,
+                    'distance' => $trip->distance,
+                    'trip_date' => $trip->trip_date,
+                    'depart_hour' => $trip->depart_hour,
+                    'end_hour' => $trip->end_hour,
+                    'price' => $trip->price,
+                    'train_err' => '',
+                    'start_from_err' => '',
+                    'end_in_err' => '',
+                    'distance_err' => '',
+                    'trip_date_err' => '',
+                    'depart_hour_err' => '',
+                    'end_hour_err' => '',
+                    'price_err' => ''
+                ];
+
+                $this->view('trips/modify', $data);
+
             }
         }
     }
