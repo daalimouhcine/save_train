5f9de9aaf59da91334a7d80e139e483cf4b9cba0
Mouhcine Daali <daalim277@gmail.com>
2022-03-02 13:52:52 +0100

    modify link of the style about clients page

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -7,7 +7,7 @@
     <title><?= SITENAME;?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/52c3d542f8.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="<?= URLROOT; ?>/style/<?= isset($_SESSION['admin_id']) ? 'admin_home' : 'style'; ?>.css">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/style/<?= isset($_SESSION['admin_id']) ? 'admin_home' : 'client_home'; ?>.css">
 </head>
 <body>
     <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
