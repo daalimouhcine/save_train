c4dfdff0383dc45095100f64b11a482ac2aa7175
Mouhcine Daali <daalim277@gmail.com>
2022-03-03 15:07:45 +0100

    display the trips that client chose and the client informations on the add reservation

--- app/controllers/Reservations.php
+++ app/controllers/Reservations.php
@@ -1,4 +1,38 @@
 <?php
     class Reservations extends Controller {
         
+        public function __construct() {
+            $this->reservationModel = $this->model('Reservation');
+            $this->userModel = $this->model('User');
+            $this->tripModel = $this->model('Trip');
+
+        }
+
+        public function index() {
+
+
+        }
+
+        public function add($trip_id) {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+            } else {
+                // Get trip and client information's
+                $trip = $this->tripModel->getOnTrip($trip_id);
+
+                // Init data
+                $data = [
+                    'trip' => $trip,
+                    'client_full_name' => $_SESSION['client_full_name'],
+                    'client_email' => $_SESSION['client_email'],
+                    'client_full_name_err' => '',
+                    'client_email_err' => '' 
+                ];
+
+                $this->view('reservations/add', $data);
+            }
+            
+            
+        }
     }
