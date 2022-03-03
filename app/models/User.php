e27115c170a58b681dbb8b3a3340288d30d0f739
Mouhcine Daali <daalim277@gmail.com>
2022-03-03 03:59:01 +0100

    try using the try catch to solve the error of the hashing password

--- app/models/User.php
+++ app/models/User.php
@@ -14,13 +14,20 @@
             $this->db->bind(':email', $email);
 
             $row = $this->db->single();
-
             $hashed_password = $row->password;
             if(password_verify($password, $hashed_password)){
               return $row;
             } else {
               return false;
             }
+            // try{
+            //     password_verify($password, $hashed_password);
+            //     return $row;
+
+            // } catch(Exception) {
+            //     return false;
+            // }
+             
         }
 
 
