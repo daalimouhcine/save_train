ae552b9fe66a0a6b66fd438ee10becb0cfa2763b
Mouhcine Daali <daalim277@gmail.com>
2022-03-05 23:59:43 +0100

    delete the join query

--- app/models/Trip.php
+++ app/models/Trip.php
@@ -34,7 +34,9 @@
 
 
         public function getOnTrip($trip_id) {
-            $this->db->query("SELECT trains.name, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE available = true");
+            $this->db->query("SELECT trains.name, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE available = true AND id = :trip_id");
+
+            $this->db->query("SELECT * FROM train_trips WHERE id = :trip_id");
             $this->db->bind(':trip_id', $trip_id);
             $trip = $this->db->single();
 
