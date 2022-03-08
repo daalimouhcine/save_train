680b224245e9a42e9dcd3b4c026b2f4017d5a3e3
Mouhcine Daali <daalim277@gmail.com>
2022-03-08 23:25:28 +0100

    delete the about page

--- app/views/home/about.php
+++
@@ -1,7 +0,0 @@
-<?php require_once APPROOT . '/views/inc/header.php'; ?>
-
-    <div class="container">
-        <h1><?= $data['content']; ?></h1>
-    </div>
-
-<?php require_once APPROOT . '/views/inc/footer.php'; ?>
