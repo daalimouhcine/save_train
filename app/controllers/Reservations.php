2087d9e04464fbbbce22ab4d0ed217efdeb186df
Mouhcine Daali <daalim277@gmail.com>
2022-03-08 23:29:01 +0100

    add the ticket download methodand delete method

--- app/controllers/Reservations.php
+++ app/controllers/Reservations.php
@@ -15,12 +15,35 @@
                 $this->view('reservations/index', $reservations);
 
             } else {
-                $this->view('reservations/');
+                $this->view('reservations/index', $reservations);
                 flash('no_reservations', 'Their is no reservations', 'alert alert-danger');
                 
             }
+        }
 
 
+        public function ticket($reservation_id) {
+            $reservation = $this->reservationModel->readOnReservation($reservation_id);
+            if($reservation) {
+                // Init data
+                $data = [
+                    'train_id' => $reservation->train_id,
+                    'trip_id' => $reservation->id,
+                    'train_name' => $reservation->name,
+                    'start_from' => $reservation->start_from,
+                    'end_in' => $reservation->end_in,
+                    'distance' => $reservation->distance,
+                    'trip_date' => $reservation->trip_date,
+                    'depart_time' => $reservation->depart_hour,
+                    'end_time' => $reservation->end_hour,
+                    'price' => $reservation->price,
+                    'reservation_date' => $reservation->reserve_time,
+                    'client_full_name' => $reservation->fullName,
+                    'client_email' => $reservation->email
+                ];
+                pdfReservation($data);
+                redirect('reservations');
+            }
         }
 
         public function add($trip_id, $client_id = null) {
@@ -33,7 +56,16 @@
 
                 // Init data
                 $data = [
-                    'trip' => $trip,
+                    'train_id' => $trip->train_id,
+                    'trip_id' => $trip->id,
+                    'train_name' => $trip->name,
+                    'start_from' => $trip->start_from,
+                    'end_in' => $trip->end_in,
+                    'distance' => $trip->distance,
+                    'trip_date' => $trip->trip_date,
+                    'depart_time' => $trip->depart_hour,
+                    'end_time' => $trip->end_hour,
+                    'price' => $trip->price,
                     'client_full_name' => $_POST['client_full_name'],
                     'client_email' => $_POST['client_email'],
                     'client_full_name_err' => '',
@@ -55,14 +87,14 @@
                 if(empty($data['client_full_name_err']) && empty($data['client_email_err'])) {
                     if($client_id != null) {
                         if($this->reservationModel->addReservation($data, $client_id)) {
-                            pdfReservation($data);
                             flash('add_reservation_success', 'Reservation added successfully');
-                            redirect('reservations/');
+                            redirect('reservations/index');
+                            pdfReservation($data);
                         }
                     } else {
                         if($this->reservationModel->addReservation($data)) {
-                            pdfReservation($data);
                             redirect('home/');
+                            pdfReservation($data);
                         }
                     }
 
@@ -91,10 +123,21 @@
                 }
 
                 $this->view('reservations/add', $data);
-            }
-            
-            
+            }   
         }
+
+
+        public function cancel($trip_id) {
+            if($this->reservationModel->cancelReservation($trip_id)) {
+                flash('reservation_cancel_success', 'Reservation canceled successfully');
+                redirect('reservations');
+            }
+        }
+
+
+
+
+
     }
 
 ?>
