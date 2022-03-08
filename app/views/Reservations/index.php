95c5dc6abdcaf2a8ab2fbef538ff664f7ee7dd3e
Mouhcine Daali <daalim277@gmail.com>
2022-03-08 23:26:45 +0100

    change the data call

--- app/views/Reservations/index.php
+++ app/views/Reservations/index.php
@@ -4,30 +4,36 @@
         <section class="trips container-fluid bg-white justify-content-center p-4 my-5">
           <div class="col-4 mx-auto text-center">
             <?php flash('no_reservations'); ?>
+            <?php flash('reservation_cancel_success'); ?>
             <?php flash('add_reservation_success'); ?>
           </div>
             
-          <?php if(is_array($data['trips']) || is_object($data['trips'])) : ?>
-            <?php foreach($data['trips'] as $trip) : ?>
+          <?php if(is_array($data) || is_object($data)) : ?>
+            <?php foreach($data as $reservation) : ?>
               <div class="col-6 bg-light d-flex flex-column text-dark card p-2 mx-auto my-5 align-items-center">
                 <div class="d-flex justify-content-center">
-                  <p class="mx-2">Train Name: <?= $trip->name; ?></p>
-                  <p class="mx-2">Date: <?= $trip->trip_date; ?></p>
+                  <p class="mx-2">Train Name: <?= $reservation->name; ?></p>
+                  <p class="mx-2">Date: <?= $reservation->trip_date; ?></p>
                 </div>
                 <div class="d-flex">
-                  <p class="m-1">From: <?= $trip->start_from; ?> &rightarrow;</p>
-                  <p class="m-1"><?= !empty($trip->distance) ? $trip->distance.'Km' : ''; ?></p>
-                  <p class="m-1"> &rightarrow;To: <?= $trip->end_in; ?></p>
+                  <p class="m-1">From: <?= $reservation->start_from; ?> &rightarrow;</p>
+                  <p class="m-1"><?= !empty($reservation->distance) ? $reservation->distance.'Km' : ''; ?></p>
+                  <p class="m-1"> &rightarrow;To: <?= $reservation->end_in; ?></p>
                 </div>
                 <div class="d-flex">
-                  <p class="m-1 mx-3">Start at: <?= $trip->depart_hour; ?></p>
-                  <p class="m-1 mx-3">End: <?= $trip->end_hour; ?></p>
+                  <p class="m-1 mx-3">Start at: <?= $reservation->depart_hour; ?></p>
+                  <p class="m-1 mx-3">End: <?= $reservation->end_hour; ?></p>
                 </div>
-                <p class="m-1">Number of Seats: <?= $trip->seat_number; ?></p>
+                <p class="m-1">Number of Seats: <?= $reservation->seat_number; ?></p>
                 <div class="d-flex align-items-center">
-                  <p class='font-weight-bold my-0 mx-3 align-self-center'><?= $trip->price; ?> DH</p>
-                  <a href="<?= URLROOT; ?>/reservations/add/<?= $trip->id; ?><?= isset($_SESSION['client_id']) ? '/'.$_SESSION['client_id'] : ''; ?>" class="btn btn-success">Reserve</a>
+                  <p class='font-weight-bold my-0 mx-3 align-self-center'><?= $reservation->price; ?> DH</p>
+                  <a href="<?= URLROOT; ?>/reservations/ticket/<?= $reservation->id; ?>" class="btn btn-outline-success">Ticket</a>
+                  <a href="<?= URLROOT; ?>/reservations/cancel/<?= $reservation->id; ?>" class="btn btn-outline-danger mx-3">Cancel</a>
                 </div>
+              </div>
+
+              <div class="container bg-light">
+
               </div>
         
             <?php endforeach; ?>
