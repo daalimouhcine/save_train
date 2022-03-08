e229291459e483c12c6e2d83123f37a56a09b40b
Mouhcine Daali <daalim277@gmail.com>
2022-03-08 23:27:37 +0100

    modify the query to macke posible if the name is not complete

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -13,14 +13,14 @@
                 switch($argCount) {
                     case 2:
                         $this->db->query('SELECT trains.*, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE start_from LIKE :from AND end_in LIKE :to AND available = true');
-                        $this->db->bind(':from', $arg[0]);
-                        $this->db->bind(':to', $arg[1]);     
+                        $this->db->bind(':from', '%'.$arg[0].'%');
+                        $this->db->bind(':to', '%'.$arg[1].'%');     
                         break;
                     
                     case 3: 
                         $this->db->query('SELECT trains.*, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE start_from LIKE :from AND end_in LIKE :to AND trip_date = :date AND available = true');
-                        $this->db->bind(':from', $arg[0]);
-                        $this->db->bind(':to', $arg[1]);
+                        $this->db->bind(':from', '%'.$arg[0].'%');
+                        $this->db->bind(':to', '%'.$arg[1].'%');
                         $this->db->bind(':date', $arg[2]);
                         break;
                 }
