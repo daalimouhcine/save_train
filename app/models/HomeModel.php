8fa091a002d2b82ff76d58499d2b55beeb1199bd
Mouhcine Daali <daalim277@gmail.com>
2022-03-13 21:37:40 +0100

    modify the query to unbroken query

--- app/models/HomeModel.php
+++ app/models/HomeModel.php
@@ -12,13 +12,13 @@
                 $argCount = count($arg);
                 switch($argCount) {
                     case 2:
-                        $this->db->query('SELECT trains.*, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE start_from LIKE :from AND end_in LIKE :to AND available = true');
+                        $this->db->query('SELECT * FROM reservations_seats WHERE start_from LIKE :from AND end_in LIKE :to AND available = true');
                         $this->db->bind(':from', '%'.$arg[0].'%');
                         $this->db->bind(':to', '%'.$arg[1].'%');     
                         break;
                     
                     case 3: 
-                        $this->db->query('SELECT trains.*, train_trips.* FROM train_trips INNER JOIN trains ON train_trips.train_id = trains.id WHERE start_from LIKE :from AND end_in LIKE :to AND trip_date = :date AND available = true');
+                        $this->db->query('SELECT * FROM reservations_seats WHERE start_from LIKE :from AND end_in LIKE :to AND trip_date = :date AND available = true');
                         $this->db->bind(':from', '%'.$arg[0].'%');
                         $this->db->bind(':to', '%'.$arg[1].'%');
                         $this->db->bind(':date', $arg[2]);
