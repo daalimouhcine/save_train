fa9287c6e8a9b95c8a495dd8f1a6d5be92d2349e
Mouhcine Daali <daalim277@gmail.com>
2022-03-14 03:27:45 +0100

    make page more responsive

--- app/views/Reservations/index.php
+++ app/views/Reservations/index.php
@@ -12,23 +12,23 @@
           <?php if(is_array($data) || is_object($data)) : ?>
             <?php foreach($data as $reservation) : ?>
               <div class="col-lg-6 col-md-8 col-sm-10 col-12 bg-light d-flex flex-column text-dark card p-2 mx-auto my-5 align-items-center">
-                <div class="d-flex justify-content-center">
+                <div class="d-flex flex-wrap justify-content-center">
                   <p class="mx-2">Train Name: <?= $reservation->name; ?></p>
                   <p class="mx-2">Date: <?= $reservation->trip_date; ?></p>
                 </div>
-                <div class="d-flex">
+                <div class="d-flex flex-wrap">
                   <p class="m-1">From: <?= $reservation->start_from; ?> &rightarrow;</p>
                   <p class="m-1"><?= !empty($reservation->distance) ? $reservation->distance.'Km' : ''; ?></p>
                   <p class="m-1"> &rightarrow;To: <?= $reservation->end_in; ?></p>
                 </div>
-                <div class="d-flex">
+                <div class="d-flex flex-wrap">
                   <p class="m-1 mx-3">Start at: <?= $reservation->depart_hour; ?></p>
                   <p class="m-1 mx-3">End: <?= $reservation->end_hour; ?></p>
                 </div>
-                <div class="d-flex align-items-center my-3">
-                  <p class='font-weight-bold my-0 mx-3 align-self-center'><?= $reservation->price; ?> DH</p>
-                  <a href="<?= URLROOT; ?>/reservations/ticket/<?= $reservation->id; ?>" class="btn btn-outline-success">Ticket</a>
-                  <a href="<?= URLROOT; ?>/reservations/cancel/<?= $reservation->id; ?>" class="btn btn-outline-danger mx-3">Cancel</a>
+                <div class="d-flex flex-wrap align-items-center justify-content-center my-3">
+                  <p class='font-weight-bold my-1 mx-3 align-self-center'><?= $reservation->price; ?> DH</p>
+                  <a href="<?= URLROOT; ?>/reservations/ticket/<?= $reservation->id; ?>" class="btn btn-outline-success my-1">Ticket</a>
+                  <a href="<?= URLROOT; ?>/reservations/cancel/<?= $reservation->id; ?>" class="btn btn-outline-danger my-1 mx-3">Cancel</a>
                 </div>
               </div>
 
