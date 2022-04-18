81074923c0ca0b769d587a772c07d71dca17d7fe
Mouhcine Daali <daalim277@gmail.com>
2022-04-18 23:39:02 +0000

    fixing error

--- app/models/User.php
+++ app/models/User.php
@@ -14,12 +14,15 @@
             $this->db->bind(':email', $email);
 
             $row = $this->db->single();
-            $hashed_password = $row->password;
-            if(password_verify($password, $hashed_password)){
-              return $row;
-            } else {
-              return false;
+            if($row) {
+                $hashed_password = $row->password;
+                if(password_verify($password, $hashed_password)){
+                    return $row;
+                    } else {
+                    return false;
+                }
             }
+            
             
             // try{
             //     password_verify($password, $hashed_password);
